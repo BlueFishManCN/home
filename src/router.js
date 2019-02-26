06c26cc01b4679dfbeca91ea4fc56c2d4d67e8ff
jerry-mac <jerrychangcn@outlook.com>
2019-02-26 15:16:41 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,22 +2,32 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: () => import('./views/Projects.vue')
+        },
+        {
+            path: '/spirit',
+            name: 'spirit',
+            component: () => import('./views/Spirit.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        }
+    ]
 })
