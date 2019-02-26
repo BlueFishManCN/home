e9140997593fc953c17ca27e2f5d1ea15cd3ec6a
jerry-mac <jerrychangcn@outlook.com>
2019-02-26 15:28:04 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
@@ -6,6 +9,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 Vue.use(ElementUI);
 
 new Vue({
