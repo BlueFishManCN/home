c99e58a70841f8cbc8f6d0c4765df4954f70e403
jerry's mac <jerrychangcn@outlook.com>
2019-12-06 13:55:32 +0800

    update

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -8,14 +8,9 @@
                             <el-link type="primary" :underline="false"
                                      href="http://biocenter.shu.edu.cn/group"><i class="el-icon-view"></i></el-link>
                         </h4>
-                        <el-carousel indicator-position="outside" height="200px">
-                            <el-carousel-item v-for="url in group">
-                                <el-image style="height:100%" :src="url"></el-image>
-                            </el-carousel-item>
-                        </el-carousel>
-                        <p>Date: Jul 2019 until now</p>
-                        <p>Creator(2): Me, Jiaming Ouyang</p>
-                        <p>Tools & Technologies: CodeIgniter, Vue.js, Element-UI</p>
+                        <div>Date: Jul 2019 until now</div>
+                        <div>Creator(2): Me, Jiaming Ouyang</div>
+                        <div>Tools & Technologies: CodeIgniter, Vue.js, Element-UI</div>
                     </el-collapse-item>
                     <el-collapse-item title="#2 Paper submission website" name="2">
                         <h4>2018 International Conference on Global Economy & Governance (GEG2018) paper submission
@@ -23,28 +18,18 @@
                             <el-link type="primary" :underline="false" href="http://geg2018.shu.edu.cn"><i
                                     class="el-icon-view"></i></el-link>
                         </h4>
-                        <el-carousel indicator-position="outside" height="200px">
-                            <el-carousel-item v-for="url in geg2018">
-                                <el-image style="height:100%" :src="url"></el-image>
-                            </el-carousel-item>
-                        </el-carousel>
-                        <p>Date: Jul 2018</p>
-                        <p>Creator(1): Me</p>
-                        <p>Tools & Technologies: CodeIgniter, Vue.js, Material Design</p>
+                        <div>Date: Jul 2018</div>
+                        <div>Creator(1): Me</div>
+                        <div>Tools & Technologies: CodeIgniter, Vue.js, Material Design</div>
                     </el-collapse-item>
                     <el-collapse-item title="#3 Dentist appointment platform" name="3">
                         <h4>Good Dentist Appointment Platform
                             <el-link type="primary" :underline="false" href="http://biocenter.shu.edu.cn/dentist_proj">
                                 <i class="el-icon-view"></i></el-link>
                         </h4>
-                        <el-carousel indicator-position="outside" height="200px">
-                            <el-carousel-item v-for="url in dentist">
-                                <el-image style="height:100%" :src="url"></el-image>
-                            </el-carousel-item>
-                        </el-carousel>
-                        <p>Date: Nov 2017 – Jul 2018</p>
-                        <p>Creator(4): Me, Jiamin Sun, Ruiying Wu, Fuzhang Yang</p>
-                        <p>Tools & Technologies: CodeIgniter, Vue.js, Element-UI</p>
+                        <div>Date: Nov 2017 – Jul 2018</div>
+                        <div>Creator(4): Me, Jiamin Sun, Ruiying Wu, Fuzhang Yang</div>
+                        <div>Tools & Technologies: CodeIgniter, Vue.js, Element-UI</div>
                     </el-collapse-item>
                     <el-collapse-item title="#4 Puzzle game" name="4">
                         <h4>MySpirit
@@ -52,11 +37,6 @@
                                      href="https://bluefishmancn.github.io/home/#/spirit">
                                 <i class="el-icon-view"></i></el-link>
                         </h4>
-                        <el-carousel indicator-position="outside" height="200px">
-                            <el-carousel-item v-for="url in spirit">
-                                <el-image style="height:100%" :src="url"></el-image>
-                            </el-carousel-item>
-                        </el-carousel>
                         <div>Date: Dec 2015 – Apr 2016</div>
                         <div>Creator(4)(Team EBM): Me, Ruizhe Hong, Kai Xie, Xiaomin Zhen</div>
                         <div>Tools & Technologies: Unity3D</div>
@@ -83,29 +63,6 @@
         data() {
             return {
                 activeName: '1',
-
-                group: [require('../assets/projects/group/group1.png'),
-                    require('../assets/projects/group/group2.png')],
-
-                geg2018: [require('../assets/projects/geg2018/geg1.png'),
-                    require('../assets/projects/geg2018/geg2.png'),
-                    require('../assets/projects/geg2018/geg3.png'),
-                    require('../assets/projects/geg2018/geg4.png'),
-                    require('../assets/projects/geg2018/geg5.png'),
-                    require('../assets/projects/geg2018/geg6.png')],
-
-                dentist: [require('../assets/projects/dentist/dentist1.png'),
-                    require('../assets/projects/dentist/dentist2.png'),
-                    require('../assets/projects/dentist/dentist3.png'),
-                    require('../assets/projects/dentist/dentist4.png'),
-                    require('../assets/projects/dentist/dentist5.png'),
-                    require('../assets/projects/dentist/dentist6.png'),
-                    require('../assets/projects/dentist/dentist7.png'),
-                    require('../assets/projects/dentist/dentist8.png')],
-
-                spirit: [require('../assets/projects/spirit/poster1.jpg'),
-                    require('../assets/projects/spirit/poster2.jpg'),
-                    require('../assets/projects/spirit/poster3.jpg')],
             };
         }
     }
